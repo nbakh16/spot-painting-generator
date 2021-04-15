b29ea08159d29e467ed4fb6815b6b63aa2641784
Nabil Akhunjee <nbakhunjee97@gmail.com>
2021-04-15 19:44:47 +0000

    improved

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ my_turtle.speed("fastest")
 my_turtle.penup()
 my_turtle.hideturtle()
 
+turtle.Screen().setup(width=700, height=700)
 turtle.colormode(255)
 
 
@@ -24,20 +25,20 @@ def spot_painting_generator(painting_size):
     gap_between_dots = size_of_dots * 3
     coordinate = -20 * painting_size
 
-    my_turtle.goto(coordinate, coordinate) #starting point
+    my_turtle.goto(coordinate, coordinate)  # starting point
 
     for i in range(painting_size):
         for j in range(painting_size):
             my_turtle.dot(size_of_dots, random_color_generator())
             my_turtle.forward(gap_between_dots)
 
-				#to start from a new line after each loop
+        # to start from a new line after each loop
         my_turtle.goto(coordinate, (my_turtle.ycor() + gap_between_dots))
 
 
-painting_size_input = int(input("Preferred painting size: "))
+# painting_size_input = int(input("Preferred painting size: "))
+painting_size_input = int(turtle.numinput(title="Spot painting", prompt="Preferred painting size(<=15): ", default=10, minval=1, maxval=15))
 
 spot_painting_generator(painting_size_input)
-
 
 turtle.Screen().exitonclick()
